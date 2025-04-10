b610242e2b03f37da551a804bc5277f8f38e2ac3
Maruthi Malapati <maruthi19955@gmail.com>
2025-04-10 23:40:08 +0530

    updated ticket form

--- src/CreateTicket.jsx
+++ src/CreateTicket.jsx
@@ -3,15 +3,23 @@ function CreateTicket() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
+  // const submitTicket = async () => {
+  //   await fetch("http://localhost:3001/tickets", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({ title, description }),
+  //   });
+  //   alert("Ticket submitted!");
+  // };
+
   const submitTicket = async () => {
-    await fetch("http://localhost:3001/tickets", {
+    await fetch("https://itsm-tool-backend-production.up.railway.app/tickets", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ title, description }),
     });
     alert("Ticket submitted!");
   };
-
   return (
     <div>
       <h2>Create Ticket</h2>
